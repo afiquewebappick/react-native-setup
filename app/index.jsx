2692d2a83ce982377169ac514b5641a6a3103f74
afiquewebappick <afique.webappick@gmail.com>
2025-12-02 17:52:11 +0600

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text className='mx-auto mt-20 text-5xl text-green-500 font-extrabold'>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
